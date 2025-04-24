60a7f8d57ef2cb42d7cd9d18cd66a5bb82209150
Khoi Le <leckhoi07@gmail.com>
2025-04-23 22:23:00 -0400

    added transformer architecture

--- model.py
+++ model.py
@@ -48,10 +48,21 @@ class TransformerModel(tf.keras.Model):
     def __init__(self):
         super().__init__()
         self.pos_encoding = PositionalEncoding(vocab_size, embed_size, seq_len)
+        self.feed_forward1 = tf.keras.layers.Dense(units=embed_size, activation='relu')
+        self.feed_forward2 = tf.keras.layers.Dense(units=embed_size, activation='relu')
+        self.norm_layer1 = tf.keras.layers.LayerNormalization()
+        self.norm_layer2 = tf.keras.layers.LayerNormalization()
+        self.attention = tf.keras.layers.MultiHeadAttention(num_attention_heads, key_dim=64)
     
     def call(self, input_seq, mask=None):
         masked_input = mask_seq(input_seq, mask)
         x = self.pos_encoding(masked_input)
+        attention_output = self.attention(x, x, x, attention_mask = mask)
+        residuals = self.norm_layer1(x + attention_output)
+        output = self.feed_forward1(residuals)
+        output = self.feed_forward2(output)
+        output = self.norm_layer2(output)
+        output = tf.nn.relu(output)
         return x
 
 sample_input = tf.constant([[1, 5, 6, 7]])
